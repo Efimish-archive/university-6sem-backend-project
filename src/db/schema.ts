4a952916d65f570ee505dae1049b66291957d4a9
Efim Ishenin <efim.ishenin@gmail.com>
2026-05-20 16:57:09 +0700

    feat: finish db schema

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -7,10 +7,14 @@ export const users = sqliteTable("users", {
   lastName: text().notNull(),
   patronymic: text(),
   email: text().notNull().unique(),
+  isSendNotify: int({ mode: "boolean" }).notNull(),
 });
 
 export const usersRelations = relations(users, ({ many }) => ({
-  roles: many(roleUser),
+  roleUser: many(roleUser),
+  customerCars: many(customerCars),
+  administratorOrders: many(orders, { relationName: "administrator" }),
+  employeeOrders: many(orders, { relationName: "employee" }),
 }));
 
 export const roles = sqliteTable("roles", {
@@ -19,7 +23,7 @@ export const roles = sqliteTable("roles", {
 });
 
 export const rolesRelations = relations(roles, ({ many }) => ({
-  users: many(roleUser),
+  roleUser: many(roleUser),
 }));
 
 export const roleUser = sqliteTable("role_user", {
@@ -48,6 +52,10 @@ export const brands = sqliteTable("brands", {
   name: text().notNull().unique(),
 });
 
+export const brandsRelations = relations(brands, ({ many }) => ({
+  cars: many(cars),
+}));
+
 export const cars = sqliteTable("cars", {
   id: int().primaryKey(),
   brandId: int()
@@ -70,6 +78,10 @@ export const services = sqliteTable("services", {
   time: int().notNull(),
 });
 
+export const servicesRelations = relations(services, ({ many }) => ({
+  orderService: many(orderService),
+}));
+
 export const customerCars = sqliteTable("customer_cars", {
   id: int().primaryKey(),
   carId: int()
@@ -82,6 +94,17 @@ export const customerCars = sqliteTable("customer_cars", {
   number: text(),
 });
 
+export const customerCarsRelations = relations(customerCars, ({ one }) => ({
+  car: one(cars, {
+    fields: [customerCars.carId],
+    references: [cars.id],
+  }),
+  customer: one(users, {
+    fields: [customerCars.customerId],
+    references: [users.id],
+  }),
+}));
+
 export const orderService = sqliteTable("order_service", {
   id: int().primaryKey(),
   serviceId: int()
@@ -91,6 +114,17 @@ export const orderService = sqliteTable("order_service", {
     .references(() => orders.id)
     .notNull(),
 });
+
+export const orderServiceRelations = relations(orderService, ({ one }) => ({
+  service: one(services, {
+    fields: [orderService.serviceId],
+    references: [services.id],
+  }),
+  order: one(orders, {
+    fields: [orderService.orderId],
+    references: [orders.id],
+  }),
+}));
 
 export const orders = sqliteTable("orders", {
   id: int().primaryKey(),
@@ -109,3 +143,21 @@ export const orders = sqliteTable("orders", {
     .notNull(),
   endDate: int({ mode: "timestamp" }).notNull(),
 });
+
+export const ordersRelations = relations(orders, ({ one, many }) => ({
+  orderService: many(orderService),
+  administrator: one(users, {
+    fields: [orders.administratorId],
+    references: [users.id],
+    relationName: "administrator",
+  }),
+  customerCar: one(customerCars, {
+    fields: [orders.customerCarId],
+    references: [customerCars.id],
+  }),
+  employee: one(users, {
+    fields: [orders.employeeId],
+    references: [users.id],
+    relationName: "employee",
+  }),
+}));
