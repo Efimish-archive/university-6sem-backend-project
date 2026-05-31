116bbaff5c3c8738a717e7516a22e73d84b72a4f
Efim Ishenin <efim.ishenin@gmail.com>
2026-05-31 23:57:57 +0700

    feat: configure testing environment

---
+++ vitest.config.ts
@@ -0,0 +1,10 @@
+import { defineConfig } from "vitest/config";
+import tsconfigPaths from "vite-tsconfig-paths";
+import { loadEnv } from "vite";
+
+export default defineConfig({
+  plugins: [tsconfigPaths()],
+  test: {
+    env: loadEnv("", process.cwd(), ""),
+  },
+});
